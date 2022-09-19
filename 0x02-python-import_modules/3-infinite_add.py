b5977a7f7678158ed55e491cd7dbd5d43daa59df
Great Okotie <greatovieokotie@gmail.com>
2022-09-19 01:15:58 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,6 @@
 <<<<<<< HEAD
 if __name__ == "__main__"
     """Print the adition of all arguments."""
-=======
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
->>>>>>> b654c02450ef75ed160d8688ae1558cfdf7ad7e8
     import sys
 
     total = 0
