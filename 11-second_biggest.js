a05f9d92b4e0fa22324ef7ee91a490d47712ceac
great200-cyber <101644848+great200-cyber@users.noreply.github.com>
2022-11-28 14:16:45 -0800

    Delete 11-second_biggest.js

--- 11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const numbers = process.argv.slice(2);
-if (numbers.length < 2) {
-  console.log(0);
-} else {
-  numbers.sort((a, b) => a - b);
-  numbers.pop();
-  console.log(numbers.pop());
-}
