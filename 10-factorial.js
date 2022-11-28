3b3cbf24edfeec4b14521a597f2c57d644e7d598
great200-cyber <101644848+great200-cyber@users.noreply.github.com>
2022-11-28 14:15:36 -0800

    Delete 10-factorial.js

--- 10-factorial.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-'use strict';
-const argv = process.argv[2];
-function factorial (n) {
-  if (isNaN(n) || n === 1) return (1);
-  else return (n * factorial(n - 1));
-}
-console.log(factorial(parseInt(argv)));
